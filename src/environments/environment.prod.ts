01ea00ab10905f6e634231f3bf1cb2df81ccb1a7
Raul Carlin <31522657+raulcarlin@users.noreply.github.com>
2019-08-26 01:53:10 -0300

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "AIzaSyDJPoUwlvqLCCHXiG9QVWWak8WXwKBGFx8",
+    apiKey: "",
     authDomain: "game-shop-10c0f.firebaseapp.com",
     databaseURL: "https://game-shop-10c0f.firebaseio.com",
     projectId: "game-shop-10c0f",
     storageBucket: "",
-    messagingSenderId: "733057553973"
+    messagingSenderId: ""
   }
 };
